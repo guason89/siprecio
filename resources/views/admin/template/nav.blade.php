efb48e59323a5f34badb737d6626867396c2a249
mtbguason@hotmail.com <mtbguason@hotmail.com>
2017-11-05 20:23:57 -0600

    quitar opciones de menu

--- resources/views/admin/template/nav.blade.php
+++ resources/views/admin/template/nav.blade.php
@@ -41,18 +41,13 @@
 
             <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
-
+                    @if(Auth::user())
                     <ul class="nav" id="side-menu">
                         <li class="sidebar-search">
-                        @if(Auth::user())
+                       
 
                             <div class="input-group custom-search-form">
-                                <input type="text" class="form-control" placeholder="Search...">
-                                <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
+                                
                             </div>
                             <!-- /input-group -->
 
@@ -60,85 +55,6 @@
 
                         <li {{ (Request::is('/') ? 'class="active"' : '') }}>
                             <a href="{{route ('admin.index') }}"><i class="fa fa-home fa-fw"></i>Inicio</a>
-                        </li>
-                        <li {{ (Request::is('*charts') ? 'class="active"' : '') }}>
-                            <a href="{{ url ('') }}"><i class="fa fa-bar-chart-o fa-fw"></i> Item 2</a>
-                            <!-- /.nav-second-level -->
-                        </li>
-                        <li {{ (Request::is('*tables') ? 'class="active"' : '') }}>
-                            <a href="{{ url ('') }}"><i class="fa fa-table fa-fw"></i> Item 3</a>
-                        </li>
-                        <li {{ (Request::is('*forms') ? 'class="active"' : '') }}>
-                            <a href="{{ url ('') }}"><i class="fa fa-edit fa-fw"></i> Item 4</a>
-                        </li>
-                        <li >
-                            <a href="#"><i class="fa fa-wrench fa-fw"></i> Elements Items<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li {{ (Request::is('*panels') ? 'class="active"' : '') }}>
-                                    <a href="{{ url ('') }}">Sub Item 1</a>
-                                </li>
-                                <li {{ (Request::is('*buttons') ? 'class="active"' : '') }}>
-                                    <a href="{{ url ('' ) }}">Sub Item 2</a>
-                                </li>
-                                <li {{ (Request::is('*notifications') ? 'class="active"' : '') }}>
-                                    <a href="{{ url('') }}">Sub Item 3</a>
-                                </li>
-                                <li {{ (Request::is('*typography') ? 'class="active"' : '') }}>
-                                    <a href="{{ url ('') }}">Sub Item 4</a>
-                                </li>
-                                <li {{ (Request::is('*icons') ? 'class="active"' : '') }}>
-                                    <a href="{{ url ('') }}"> Sub Item 5</a>
-                                </li>
-                                <li {{ (Request::is('*grid') ? 'class="active"' : '') }}>
-                                    <a href="{{ url ('') }}">Sub Item 6</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-sitemap fa-fw"></i> Multi-Level Dropdown<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li>
-                                    <a href="#">Second Level Item</a>
-                                </li>
-                                <li>
-                                    <a href="#">Second Level Item</a>
-                                </li>
-                                <li>
-                                    <a href="#">Third Level <span class="fa arrow"></span></a>
-                                    <ul class="nav nav-third-level">
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                        <li>
-                                            <a href="#">Third Level Item</a>
-                                        </li>
-                                    </ul>
-                                    <!-- /.nav-third-level -->
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                        <li>
-                            <a href="#"><i class="fa fa-files-o fa-fw"></i> Item 5<span class="fa arrow"></span></a>
-                            <ul class="nav nav-second-level">
-                                <li {{ (Request::is('*blank') ? 'class="active"' : '') }}>
-                                    <a href="{{ url ('') }}">Blank Page</a>
-                                </li>
-                                <li>
-                                    <a href="{{ url ('') }}">Login Page</a>
-                                </li>
-                            </ul>
-                            <!-- /.nav-second-level -->
-                        </li>
-                        <li {{ (Request::is('*documentation') ? 'class="active"' : '') }}>
-                            <a href="{{ url ('') }}"><i class="fa fa-file-word-o fa-fw"></i> Item 6</a>
                         </li>
                     </ul>
                     @endif
